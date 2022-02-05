47fa634192f33eb0f00bb963ef79b45f8f0596c7
Radames Ajna <radamajna@gmail.com>
2022-02-05 00:23:42 -0800

    vercel config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 export default {
   telemetry: false,
+  target: 'static',
   /*
    ** Headers of the page
    */
@@ -7,14 +8,14 @@ export default {
     base: '/traduzirse/'
   },
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'traduzir-se',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'traduzir-se'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
